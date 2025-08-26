323b9999969b866627066de14a9758326133dada
Manavj99 <97878307+Manavj99@users.noreply.github.com>
2025-08-26 12:13:15 -0400

    Update firebase.js

--- src/components/services/firebase.js
+++ src/components/services/firebase.js
@@ -5,13 +5,7 @@ import { getDatabase } from 'firebase/database';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCcH4zneT73CvuGb26-XxpKwRGfN_X5qUo",
-    authDomain: "shiftease-49cf2.firebaseapp.com",
-    projectId: "shiftease-49cf2",
-    storageBucket: "shiftease-49cf2.appspot.com",
-    messagingSenderId: "784573676526",
-    appId: "1:784573676526:web:15716eb51b90bf9a9decff",
-    measurementId: "G-5GNLW6YXP3"
+
 };
 
 const app = initializeApp(firebaseConfig);
