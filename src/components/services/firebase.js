f22d8d4cedf78b61e79ea6f96fd31dca18f485cd
Manavj99 <97878307+Manavj99@users.noreply.github.com>
2025-08-26 12:33:01 -0400

    Updates to firebase

--- src/components/services/firebase.js
+++ src/components/services/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCcH4zneT73CvuGb26-XxpKwRGfN_X5qUo",
-    authDomain: "shiftease-49cf2.firebaseapp.com",
-    projectId: "shiftease-49cf2",
-    storageBucket: "shiftease-49cf2.appspot.com",
-    messagingSenderId: "784573676526",
-    appId: "1:784573676526:web:15716eb51b90bf9a9decff",
-    measurementId: "G-5GNLW6YXP3"
+ 
   };
   
 
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,13 +5,7 @@ import { getAuth } from 'firebase/auth';
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCcH4zneT73CvuGb26-XxpKwRGfN_X5qUo",
-    authDomain: "shiftease-49cf2.firebaseapp.com",
-    projectId: "shiftease-49cf2",
-    storageBucket: "shiftease-49cf2.appspot.com",
-    messagingSenderId: "784573676526",
-    appId: "1:784573676526:web:15716eb51b90bf9a9decff",
-    measurementId: "G-5GNLW6YXP3"
+ 
   };
   
 
